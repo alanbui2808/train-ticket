3f8082bac1d4658344ece8488d3124d94bf0f5a3
Alan <buihanquocanh@gmail.com>
2025-04-10 01:02:17 -0500

    Log the config out inside initializer.Run()

--- internal/initialize/run.go
+++ internal/initialize/run.go
@@ -1,10 +1,20 @@
 package initialize
 
+import (
+	"fmt"
+
+	"github.com/alanbui/train-ticket/global"
+	"go.uber.org/zap"
+)
+
 // contains all the initializers
 func Run() {
 	// load config
 	LoadConfig()
+	m := global.Config.Mysql
+	fmt.Println("Loading config mysql", m.Username, m.Password)
 	InitLogger()
+	global.Logger.Info("Config Log ok!!", zap.String("ok", "success"))
 	InitMySql()
 	InitRedis()
 
