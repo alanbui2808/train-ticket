6536f3ff361c607a6572f3e4bb87966b5c6a6d4b
Alan <buihanquocanh@gmail.com>
2025-04-12 18:05:46 -0500

    Define `global.Rdb` to be `*redis.Client`

--- global/global.go
+++ global/global.go
@@ -3,6 +3,7 @@ package global
 import (
 	"github.com/alanbui/train-ticket/package/logger"
 	"github.com/alanbui/train-ticket/package/setting"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
@@ -10,6 +11,7 @@ var (
 	Config setting.Config    // stores all the config
 	Logger *logger.LoggerZap // global logger
 	Mdb    *gorm.DB
+	Rdb    *redis.Client
 )
 
 /*
