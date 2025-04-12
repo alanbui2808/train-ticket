64671bfffa6b527aa7bc713ffa6a04f0bf3207a3
Alan <buihanquocanh@gmail.com>
2025-04-11 19:09:40 -0500

    add `*gorm.DB` inside `global`

--- global/global.go
+++ global/global.go
@@ -3,11 +3,13 @@ package global
 import (
 	"github.com/alanbui/train-ticket/package/logger"
 	"github.com/alanbui/train-ticket/package/setting"
+	"gorm.io/gorm"
 )
 
 var (
 	Config setting.Config    // stores all the config
 	Logger *logger.LoggerZap // global logger
+	Mdb    *gorm.DB
 )
 
 /*
