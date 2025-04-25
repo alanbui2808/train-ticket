2ced4e9a3dc1416726e3c3aacffe71d1f2c0029b
Alan <buihanquocanh@gmail.com>
2025-04-25 00:34:10 -0500

    generate `GORM` table with `Gen` on table `go_crm_user`

--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -8,6 +8,7 @@ import (
 	"github.com/alanbui/train-ticket/internal/po"
 	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
+	"gorm.io/gen"
 	"gorm.io/gorm"
 )
 
@@ -44,7 +45,8 @@ func InitMySql() {
 
 	// Set Pool
 	SetPool()
-	migrateTables()
+	genTableDAO()
+	// migrateTables()
 }
 
 // Connect to Pooling settings
@@ -61,6 +63,27 @@ func SetPool() {
 	sqlDB.SetMaxOpenConns(m.MaxOpenConns)
 	sqlDB.SetConnMaxLifetime(time.Duration(m.ConnMaxLifetime))
 
+}
+
+func genTableDAO() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "./internal/model",
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+	})
+
+	// // gormdb, _ := gorm.Open(mysql.Open("root:@(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"))
+	// g.UseDB(gormdb) // reuse your gorm db
+	g.UseDB(global.Mdb)
+	g.GenerateModel("go_crm_user")
+
+	// // Generate basic type-safe DAO API for struct `model.User` following conventions
+	// g.ApplyBasic(model.User{})
+
+	// // Generate Type Safe API with Dynamic SQL defined on Querier interface for `model.User` and `model.Company`
+	// g.ApplyInterface(func(Querier) {}, model.User{}, model.Company{})
+
+	// Generate the code
+	g.Execute()
 }
 
 // Create a table in MySQL
